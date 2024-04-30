9e3bbf026a6987087fff68d1939d80c7d5c2615f
Vojtoo <114012604+Vojtoo@users.noreply.github.com>
2024-04-30 23:55:48 +0200

    upravena query na  maine
    upravena query na  maine, prechody na main ked sa klikne na logo a nazov na main navbare

--- mobiletech/app/Http/Controllers/MainController.php
+++ mobiletech/app/Http/Controllers/MainController.php
@@ -9,9 +9,13 @@ use Illuminate\Http\Request;
 class MainController extends Controller
 {
     //show products on main
-    public function show_top_products(){
+    public function show_top_products()
+    {
 
-        $products = Product::join('images','products.id','=','images.product_id')->select('products.*', 'images.*')->limit(3)->get();
+        $single_images_for_each_product = Image::selectRaw('MIN(id) as min_id')->groupBy('product_id')->get();
+        $products = Product::join('images', 'products.id', '=', 'images.product_id')
+            ->select('products.*', 'images.*')
+            ->whereIn('images.id', $single_images_for_each_product)->limit(3)->get();
 
         return view('pages/mainWithProducts', ['products' => $products]);
     }
