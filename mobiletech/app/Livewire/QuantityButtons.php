9ee4faf1796d73f2b95275974d0ab314afb4064d
Vojtoo <114012604+Vojtoo@users.noreply.github.com>
2024-05-04 12:43:00 +0200

    pridavanie aj na detaili
    treba to este trosku upratan nech je dodrzane DRY

---
+++ mobiletech/app/Livewire/QuantityButtons.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Product;
+use Livewire\Component;
+
+class QuantityButtons extends Component
+{
+    public $product_id;
+    public $quantity = 1;
+    public $image;
+    public $price;
+    public function render()
+    {
+        return view('livewire.quantity-buttons');
+    }
+
+    public function add(){
+        $this->quantity++;
+        return view('livewire.quantity-buttons');
+    }
+
+    public function sub(){
+        if ($this->quantity > 1){
+            $this->quantity--;
+        }
+        return view('livewire.quantity-buttons');
+    }
+
+    public function submit()
+    {
+        // ziskanie kvantity produktu
+        $product = Product::where('id', $this->product_id)->get()->first();
+        // pocet dostupnych produktov
+        $available_quantity = $product->stock_quantity;
+
+        $product_added = false;
+
+        if (session()->has('cart')) {
+            // ziskanie kosika z relacie
+            $current_cart = session()->get('cart');
+
+            // kontrola ci uz je produkt v kosiku
+            if (array_key_exists($this->product_id, $current_cart)) {
+
+                // kontrola ci sa neprekrocil pocet produktov "na sklade"
+                if ($current_cart[$this->product_id]['quantity'] + $this->quantity <= $available_quantity) {
+                    //zmena kvantity pre zaznam v kosiku
+                    $current_cart[$this->product_id] = ['quantity' => $this->quantity + $current_cart[$this->product_id]['quantity'], 'image' => $this->image, 'price' => $this->price];
+                    $product_added = true;
+                }
+
+            } else {
+                // kontrola ci sa neprekrocil pocet produktov "na sklade"
+                if ($this->quantity <= $available_quantity) {
+                    //zmena kvantity pre zaznam v kosiku
+                    $current_cart[$this->product_id] = ['quantity' => $this->quantity, 'image' => $this->image, 'price' => $this->price];
+                    $product_added = true;
+                }
+            }
+
+
+            session(['cart' => $current_cart]);
+
+        } else {
+            // vytvorenie pola poli, ktore reprezentujenakubny kosik
+
+
+            // kontrola ci sa neprekrocil pocet produktov "na sklade"
+            if ($this->quantity <= $available_quantity) {
+                session(['cart' => array($this->product_id => ['quantity' => $this->quantity, 'image' => $this->image, 'price' => $this->price])]);
+                $product_added = true;
+            }
+        }
+
+        // ak bol produkt pridany vygeneruje sa signal na prepocitanie celkoveho suctu poloziek v kosiku
+        if($product_added){
+            $this->quantity =1;
+            $current_cart = session()->get('cart');
+            $this->dispatch('totalSumChanged', $this->product_id, $this->price, $this->quantity);
+        }
+
+        return view('livewire.quantity-buttons');
+    }
+}
--- mobiletech/resources/views/pages/productDetail.blade.php
+++ mobiletech/resources/views/pages/productDetail.blade.php
@@ -22,7 +22,7 @@
                                     <h1 class="pb-2">Popis produktu</h1>
                                     <p>{{$product_parameters->first()->description}}</p>
                                     <h1 class="py-2">Parametre</h1>
-                                    @for($i = 0; $i < 5; $i++)
+                                    @for($i = 0; $i < sizeof($product_parameters); $i++)
                                         <div class="row">
                                             <div class="col-6">{{$product_parameters[$i]->scp_name}}</div>
                                             <div class="col-6">{{$product_parameters[$i]->value}}</div>
@@ -40,30 +40,18 @@
                             </div>
                         </li>
                         <li>
-                            <div class="row">
-                                <div class="col-12 col-sm-6 justify-content-center">
-                                    <div class="input-group quantity-input-group">
-                                        <button class="btn btn-outline-secondary" type="button">-</button>
-                                        <input type="text" id="quantity" class="form-control text-center" value="1"
-                                               aria-label="Quantity">
-                                        <span class="input-group-text">ks</span>
-                                        <button class="btn btn-outline-secondary" type="button">+</button>
-                                    </div>
 
-                                </div>
-                                <div class="col-12 col-sm-6 py-sm-0 py-3 justify-content-center">
-                                    <button class="btn cart-btn cart-btn-fixed-width" >
-                                        <img src="{{asset('resources/icons/shopping_cart.svg')}}" alt="nakupny kosik">
-                                        Vložiť do košíka
-                                    </button>
-                                </div>
-                            </div>
+                            @livewire('quantity-buttons',['product_id' => $product_parameters->first()->product_id, 'image' => $product_images[0]->name_hash, 'price' =>$product_parameters->first()->price])
+
+
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
+
+
 @endsection
 
 @section('offcanvas')
