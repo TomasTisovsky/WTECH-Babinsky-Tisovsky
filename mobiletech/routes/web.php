ded87a06fc3f42aa0612ba5dbaa17201bed90637
Vojtoo <114012604+Vojtoo@users.noreply.github.com>
2024-05-06 12:57:00 +0200

    odkomentovany '/' route

--- mobiletech/routes/web.php
+++ mobiletech/routes/web.php
@@ -24,7 +24,7 @@ require __DIR__.'/auth.php';
 require __DIR__.'/admin.php';
 
 // Hlavna stranka
-#Route::get('/', [MainController::class, 'show_top_products']);
+Route::get('/', [MainController::class, 'show_top_products']);
 
 
 // Detail produktu
