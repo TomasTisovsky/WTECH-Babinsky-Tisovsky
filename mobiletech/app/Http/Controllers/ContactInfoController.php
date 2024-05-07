d8d716cfb793dd2e438f353f5e8716fd2a8a7c86
Vojtoo <114012604+Vojtoo@users.noreply.github.com>
2024-05-07 17:55:59 +0200

    kont udaje kosik
    v kosiku musi byt aspon 1 vec aby sa dalo ist dalej

--- mobiletech/app/Http/Controllers/ContactInfoController.php
+++ mobiletech/app/Http/Controllers/ContactInfoController.php
@@ -8,19 +8,26 @@ use Illuminate\Support\Facades\Auth;
 class ContactInfoController extends Controller
 {
 
-    public function enter_customer_information(){
+    public function enter_customer_information()
+    {
 
-        // kontrola ci ide o prihlaseneho pouzivatela
-        
-        if (Auth::check()){
-            $user = Auth::user();
-            return view('pages/customerInformationAuth', compact('user'));
-        }else{
-            return view('pages/customerInformation');
+        // kontrola ci jestvuje nakupny kosik
+        if (session()->has('cart')) {
+            if (count(session('cart'))> 0) {
+                if (Auth::check()) {
+                    // kontrola ci ide o prihlaseneho pouzivatela
+                    $user = Auth::user();
+                    return view('pages/customerInformationAuth', compact('user'));
+                } else {
+                    return view('pages/customerInformation');
+                }
+            }
+
         }
+        return redirect()->route('shopping-cart-view');
+
+
     }
-
-
 
 
 }
