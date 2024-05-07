425c969f5b2432b717e851c3bc3c2aeed50da813
Vojtoo <114012604+Vojtoo@users.noreply.github.com>
2024-05-07 14:40:59 +0200

    priprava mala

---
+++ mobiletech/resources/views/partials/payment-transport-stage.blade.php
@@ -0,0 +1,10 @@
+<section class="col-12  buying-stage-title">
+    <h2 class="w-100">Tvoj nákupný košík</h2>
+</section>
+
+<section class="row container-fluid buying-stage-bar">
+    <div class="col-md-3 col-sm-6 buying-stage passed-buying-stage"><span>Nákupný košík</span></div>
+    <div class="col-md-3 col-sm-6 buying-stage passed-buying-stage"><span>Kontaktné informácie</span></div>
+    <div class="col-md-3 col-sm-6 buying-stage current-buying-stage"><span>Doprava a platba</span></div>
+    <div class="col-md-3 col-sm-6 buying-stage"><span>Dokončenie objednávky</span></div>
+</section>
