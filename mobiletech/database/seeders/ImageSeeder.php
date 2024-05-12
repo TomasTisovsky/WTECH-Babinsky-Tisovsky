5c3b7e31e85e1bc079fc490da891a77ba4244ee4
Vojtoo <114012604+Vojtoo@users.noreply.github.com>
2024-05-12 22:15:46 +0200

    sicke produkty

--- mobiletech/database/seeders/ImageSeeder.php
+++ mobiletech/database/seeders/ImageSeeder.php
@@ -94,6 +94,40 @@ class ImageSeeder extends Seeder
 
             ]);
 
+        }
+
+        // obrazky pre prislusenstvo
+        $opened_acc_data_file = fopen('public/resources/prislusenstvo_data.csv','r');
+        fgetcsv($opened_acc_data_file);
+        for ($i = 39; $i <= 42; $i++){
+            $phone_data = fgetcsv($opened_acc_data_file);
+
+            Image::create([
+                'product_id' => $i,
+                'image_name' => $phone_data[4],
+                'created_at' => now(),
+                'updated_at' => now(),
+
+            ]);
+
+
+
+        }
+
+        // obrazky pre obaly
+        $opened_acc_data_file = fopen('public/resources/obaly_data.csv','r');
+        fgetcsv($opened_acc_data_file);
+        for ($i = 43; $i <= 46; $i++){
+            $phone_data = fgetcsv($opened_acc_data_file);
+
+            Image::create([
+                'product_id' => $i,
+                'image_name' => $phone_data[4],
+                'created_at' => now(),
+                'updated_at' => now(),
+
+            ]);
+
 
 
         }
