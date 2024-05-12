3d70235800ac65b41818e5b9109c74568eede868
Tomáš Tisovský <tisovsky30@gmail.com>
2024-05-12 15:14:00 +0200

    upravy

--- mobiletech/routes/web.php
+++ mobiletech/routes/web.php
@@ -8,8 +8,6 @@ use App\Http\Controllers\AdminPanelMainController;
 use App\Http\Controllers\AddProductController;
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProductDetailController;
-use \App\Http\Controllers\CartController;
-use \App\Http\Controllers\ProductController;
 
 // prihlasenie
 Route::get('/dashboard', function () {
@@ -45,8 +43,3 @@ Route::get('/shopping-cart', [CartController::class, 'view_shopping_cart'])->nam
 Route::get('/deb', [CartController::class, 'debug'])->name('shopping-cart-debug.add'); // ukaze obsah kosika
 Route::get('/ns', [CartController::class, 'new_session'])->name('new-session'); // resetuje session
 
-
-//product search route  
-Route::get('/search', [ProductController::class, 'search'])->name('search');
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
